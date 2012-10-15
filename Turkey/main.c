6ab6c5661878d49aee2a3f70aed9759ca3333c90
Uchendu Nwachuku <developer@unndunn.com>
2012-10-15 18:26:19 -0400

    Initial Commit

---
+++ Turkey/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Turkey
+//
+//  Created by Uchendu Nwachuku on 10/15/12.
+//  Copyright (c) 2012 Uchendu Nwachuku. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
